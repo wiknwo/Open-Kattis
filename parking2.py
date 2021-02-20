32b3935e72ae5da8121044f1ee511bbb178a7199
wiknwo <spinningthewww@outlook.com>
2021-02-20 05:19:35 +0000

    Accepted

--- parking2.py
+++ parking2.py
@@ -22,24 +22,6 @@ def main():
         positions_on_longstreet = [int(x) for x in positions_on_longstreet]
         distance = max(positions_on_longstreet) - min(positions_on_longstreet)
         print(distance * 2)
-    # t = int(input("")) # number of test cases
-    
-    # # Get input from user
-    # for i in range(0, t):
-    #     n = input("") # number of stores Michael wants to visit
-    #     positions_on_longstreet = input("").split(" ")
-    #     positions_on_longstreet = list(map(int, positions_on_longstreet))
-    #     positions_on_longstreet.sort() # Sort the street
-
-    #     # Calculate minimum distance Michael walks and 
-    #     # subtract first position from sum because that is 
-    #     # where Michael parked
-    #     minimum_walking_distance = 0         
-    #     for i in range(1, len(positions_on_longstreet)):
-    #         minimum_walking_distance += (positions_on_longstreet[i] - positions_on_longstreet[i - 1])
-    #     minimum_walking_distance += (positions_on_longstreet[i] - positions_on_longstreet[0])    
-    #     print(minimum_walking_distance)
-    #     minimum_walking_distance = 0
-    
+        
 if __name__ == '__main__':
     main()
