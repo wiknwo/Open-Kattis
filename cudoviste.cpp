fdab0024bc30802d908aef4149751965e256f058
Bill-IN <will2@outlook.ie>
2021-02-20 19:37:30 +0000

    Accepted

--- cudoviste.cpp
+++ cudoviste.cpp
@@ -1,31 +1,79 @@
 /**
- * William Ikenna-Nwosu (16303711)
- **/
-#include <iostream>
-#include <map>
-#include <string>
-#include <sstream>
+ * Easy
+ *
+ *
+ * # = building
+ * X = parked car
+ * . = free space
+ * monster truck is 2 * 2
+ */
+#include<iostream>
 using namespace std;
 
-int main(){
-    map<int, int> numberOfParkingSpaces = {{0, 0}, {1, 0}, {2, 0}, {3, 0}, {4, 0}};
-    string monstertruck[2][2];
-    int r = 0;
-    int c = 0;
+void getSquashCount(char monsterTruckSpace[], int carsSquashed[]);
 
-    cout << "Enter number of rows and columns separated by space: ";
-    cin >> r >> c;
-    string parkinglot[r][c];
-    string lineofinput = "";
+int main()
+{
+	int R = 0; //Number of rows.
+	int C = 0; //Number of columns.
+	int rowNumber, columnNumber;
+	static int squashCount[5] = {}; //initialized with 0.
+	static char twoByTwo[4]; //Monster truck parking space.
 
-    for(int i = 0;i < r;i++){
-        cout << "Enter parking line: ";
-        cin >> lineofinput;
-        stringstream(lineofinput);
-        cout << lineofinput << endl;
-    }
+	cin >> R >> C;
 
-    for(int i = 0;i < r;i++){
-        cout << parkinglot[i] << endl;
-    }
+	//Filling map of city.
+	string mapOfCity[R];
+
+	for(rowNumber = 0;rowNumber < R;rowNumber++)
+	{
+		cin >> mapOfCity[rowNumber];
+	}
+
+	//Iterating over map of city and determining number of parking spaces for monster truck.
+	for(rowNumber = 0;rowNumber < R - 1;rowNumber++)
+	{
+		for(columnNumber = 0;columnNumber < C - 1;columnNumber++)
+		{
+			//Monster truck parking space.
+			twoByTwo[0] = mapOfCity[rowNumber][columnNumber];
+			twoByTwo[1] = mapOfCity[rowNumber + 1][columnNumber];
+			twoByTwo[2] = mapOfCity[rowNumber][columnNumber + 1];
+			twoByTwo[3] = mapOfCity[rowNumber + 1][columnNumber + 1];
+			getSquashCount(twoByTwo, squashCount);
+		}
+	}
+
+	//Print required output.
+	for(int i = 0;i < 5;i++)
+		cout << squashCount[i] << "\n";
+}
+
+void getSquashCount(char monsterTruckSpace[], int carsSquashed[])
+{
+	int numFreeSpaces = 0, numParkedCars = 0;
+
+	for(int i = 0;i < 4;i++)
+	{
+		if(monsterTruckSpace[i] == '.')
+			numFreeSpaces++;
+
+		else if(monsterTruckSpace[i] == 'X')
+			numParkedCars++;
+	}
+
+	if(numFreeSpaces == 4)
+		carsSquashed[0] += 1;
+
+	else if((numParkedCars == 1) && (numFreeSpaces == 3))
+		carsSquashed[1] += 1;
+
+	else if((numParkedCars == 2) && (numFreeSpaces == 2))
+		carsSquashed[2] += 1;
+
+	else if((numParkedCars == 3) && (numFreeSpaces == 1))
+		carsSquashed[3] += 1;
+
+	else if(numParkedCars == 4)
+		carsSquashed[4] += 1;
 }
